c62f0ccd97a6e3573ff88fabc2096fb877808834
Lipe Marcz <lipelimamarcz@gmail.com>
2025-10-15 11:36:43 -0300

    Um pouco de html + Urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from core.models import Evento
 # Create your views here.
+
+# def index(request):
+   #return redirect('/agenda/')
+ 
+def lista_eventos(request):
+    usuario = request.user
+    if not usuario.is_authenticated:
+        return HttpResponse('Usuário não autenticado')
+    evento = Evento.objects.all()
+    response = {'eventos': evento}
+    return render(request, 'agenda.html', response)
+
+def local_evento(request, titulo_evento):
+    try:
+        evento = Evento.objects.get(titulo=titulo_evento)
+        return HttpResponse(f'Evento: {evento.titulo} - Descrição: {evento.descricao} - Data do Evento: {evento.data_evento}')
+    except Evento.DoesNotExist:
+        raise Http404("Evento não encontrado")
